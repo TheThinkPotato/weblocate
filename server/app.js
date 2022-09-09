da46d4ce589a5d805c4a9205d434199cd1c8ae5b
TheThinkPotato <dan.g.lopez@outlook.com>
2022-09-09 15:11:50 +1000

    :bug: changed default host port to port 3000

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 require("dotenv").config();
 
 const hostname = process.env.HOST_NAME || "127.0.0.1";
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 // Serve out any static assets correctly
 app.use(express.static('../client/build'))
